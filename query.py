fe7a051fc02f01dc124146b9f164d210a4d2f616
jbakken16 <51429777+jbakken16@users.noreply.github.com>
2023-03-23 19:22:29 -0700

    added docstrings

--- query.py
+++ query.py
@@ -1,38 +1,44 @@
-import os
 import dimcli
-from dimcli.utils import *
 import pandas as pd
 import numpy as np 
+import re
 
-# TODO: regex the dates in extract_year
-# TODO: separate results into folders
-# TODO: run the negative queries with partial string match
 
 class InvalidQueryTypeException(Exception):
     "Raised when the query type is not 'exact' or 'fuzzy'"
     pass
 
-def login():
-
-    dimcli.login(key="02CB1C5A52B5430AA173E8D88F59A986",  
-                 endpoint="https://app.dimensions.ai/api/dsl/v2")
-
+def login(key_in="02CB1C5A52B5430AA173E8D88F59A986", endpoint_in="https://app.dimensions.ai/api/dsl/v2"):
+    """ Inputs: key, endpoint 
+        Outputs: Dimcli DSL query client
+    """
+    dimcli.login(key=key_in, endpoint=endpoint_in)
     return dimcli.Dsl()
 
 def format_title(title):
-
-    bad_punctuation = [':', '[', ']', '"']
+    """ Input: title from df 
+        Outputs: string title with colons, brackets and quotation marks removed
+    """
+    bad_punc = [':', '[', ']', '"']
     title_str = str(title)
 
-    for p in bad_punctuation:
+    for p in bad_punc:
         title_str = title_str.replace(p,"")
 
     return title_str
 
+# TODO: regex the dates in extract_year
 def extract_year(date):
-    return int(date[-5:-1])
+    """ Input: string date
+        Output: string year
+    """
+    regex = "\d\d\d\d"
+    return re.search(regex, date).group()
 
 def query_exact_title_year(title, year):
+    """ Input: String title, string year
+        Output: Query string to query for an exact title and year match
+    """
     exact_title = '"' + '\\"' + title + '\\"' + '"'
     query = f"""
         search publications in title_only 
@@ -43,6 +49,9 @@ def query_exact_title_year(title, year):
     return query
 
 def query_fuzzy_title_year(title, year):
+    """ Input: String title, string year
+        Output: Query string to query for a fuzzy title and year match
+    """
     query = f"""
         search publications in title_only 
             for "{dsl_escape(title)}" 
@@ -52,6 +61,9 @@ def query_fuzzy_title_year(title, year):
     return query
 
 def query_fuzzy_title_year_author(title, year, author):
+    """ Input: String title, string year
+        Output: Query string to query for a fuzzy title, year, and author match
+    """
     query = f"""
         search publications in title_only 
             for "{dsl_escape(title)}" 
@@ -62,17 +74,23 @@ def query_fuzzy_title_year_author(title, year, author):
     return query
 
 def run_query(df, q_type="exact"):
-
+    """ Input: dataframe of references
+        Param: query type, "exact" or "fuzzy"
+        Outputs:
+            positive_results - dataframe of positive query results
+            negative_results - dataframe of references that returned a null query result
+            dateless - dataframe of references that have no date
+            bad_format - dataframe of references with bad title or date formatting
+    """
     dsl = login()
-    df_titles_years = df[["Title", "Date"]]
 
-    # TODO: turn into dataframes, use for output
-    query_results = []
-    yes_result = []
-    no_result = []
-    no_date = []
-    bad_format = []
-    results_df = pd.DataFrame()
+    query_result_array = []
+    positive_results = pd.DataFrame()
+    negative_results = pd.DataFrame()
+    dateless = pd.DataFrame()
+    bad_format = pd.DataFrame()
+
+    df_titles_years = df[["Title", "Date"]]
 
     for index, [title, date] in df_titles_years.iterrows():
 
@@ -85,7 +103,7 @@ def run_query(df, q_type="exact"):
         try:
             # separate dateless refs
             if date == ' ':
-                no_date.append([title,date])
+                dateless = pd.concat([dateless, df.iloc[index].to_frame().T], axis=0)
                 continue
             else:
                 # remove colons/brackets    
@@ -93,10 +111,11 @@ def run_query(df, q_type="exact"):
                 # format YYYY
                 year = extract_year(date)
                 # remove unreasonable years  
-                if year < 1800 or year > 2023:
+                if int(year) < 1800 or int(year) > 2023:
                     raise Exception
+
         except Exception:
-            bad_format.append([title,date])
+            bad_format = pd.concat([bad_format, df.iloc[index].to_frame().T], axis=0)
             continue
 
         print("\n" + str(index) + "/" + str(num_rows))
@@ -107,10 +126,9 @@ def run_query(df, q_type="exact"):
             if q_type == "exact":
                 query = query_exact_title_year(title, year)
             elif q_type == "fuzzy":
-                query = query_fuzzy_title(title, year)
+                query = query_fuzzy_title_year(title, year)
             else:
                 raise InvalidQueryTypeException
-
         except InvalidQueryTypeException:
             print("Invalid query type, retry with valid q_type string")
             break
@@ -119,24 +137,43 @@ def run_query(df, q_type="exact"):
 
         # Separate into succesful/unsuccesful references
         if query_df is None:
-            no_result.append([title,year]) # df.iloc[index]
+            negative_results = pd.concat([negative_results, df.iloc[index].to_frame().T], axis=0)
         elif len(query_df) == 0:
-            no_result.append([title,year])
+            negative_results = pd.concat([negative_results, df.iloc[index].to_frame().T], axis=0)
         else:
-            yes_result.append([title,year])
-            query_results.append(query_df)
+            query_result_array.append(query_df)
 
+    if len(query_result_array) != 0:
+        positive_results = pd.concat(query_result_array).drop_duplicates(subset='id')
 
-    results_df = pd.concat(query_results).\
-                drop_duplicates(subset='id')
-    
-    print('Yes: ' + str(len(yes_result)))
-    print('No: ' + str(len(no_result)))
-    print('Dateless: ' + str(len(no_date)))
+    print('Yes: ' + str(len(positive_results)))
+    print('No: ' + str(len(negative_results)))
+    print('Dateless: ' + str(len(dateless)))
     print('Bad Format: ' + str(len(bad_format)))
     
-    return results_df, yes_result, no_result, no_date, bad_format
+    return positive_results, negative_results, dateless, bad_format
 
 def query_by_path(csv_path, q_type="exact"):
+    """ Input: string of path to csv
+        Param: query type, "exact" or "fuzzy"
+        Output: run_query function using dataframe of references from path
+    """
     return run_query(pd.read_csv(csv_path), q_type=q_type)
+
+def convert_results_to_csv(query_results, report):
+    '''Converts query results into csv files and separates into respective folders
+        Input: query_results, the dataframes returned by the run_query function
+        Input: report, the queried report formatted as a string 'report_title.csv'
+    '''
+    positive_results, negative_results, dateless, bad_format = query_results
+
+    if len(positive_results) > 0:
+        positive_results.to_csv('dsl_query_results/positive_results/positive_' + report)
+    if len(negative_results) > 0:
+        negative_results.to_csv('dsl_query_results/negative_results/negative_' + report)
+    if len(dateless) > 0:
+        dateless.to_csv('dsl_query_results/dateless/dateless_' + report)
+    if len(bad_format) > 0:
+        bad_format.to_csv('dsl_query_results/bad_format/bad_format_' + report)
+    return  
     
